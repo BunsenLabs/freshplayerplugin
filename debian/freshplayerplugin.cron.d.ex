f2960875ae481a49a0823be35807d85dd14238e1
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2015-07-06 22:14:44 +0200

    First package

--- debian/freshplayerplugin.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the freshplayerplugin package
-#
-0 4	* * *	root	[ -x /usr/bin/freshplayerplugin_maintenance ] && /usr/bin/freshplayerplugin_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(freshplayerplugin):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="freshplayerplugin" command="/usr/bin/freshplayerplugin"
