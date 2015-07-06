c55ed27ea3143c7d87e0b91c43ed6d23ffce6eee
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2015-07-06 21:45:00 +0200

    dh_make invocation

---
+++ debian/freshplayerplugin.default.ex
@@ -0,0 +1,10 @@
+# Defaults for freshplayerplugin initscript
+# sourced by /etc/init.d/freshplayerplugin
+# installed at /etc/default/freshplayerplugin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(freshplayerplugin):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="freshplayerplugin" command="/usr/bin/freshplayerplugin"
